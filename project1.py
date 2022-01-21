f20b820470692b31c80bc1a9c291e600cb1b56d8
Martin <mwgoff24@students.bmchs.org>
2022-01-21 09:48:10 -0600

    Started work on first project

---
+++ project1.py
@@ -0,0 +1,19 @@
+print("A Mad Libbed Concert: The Maybe Funny Story")
+print("Welcome to this mad lib game!")
+print("I will ask for some words for you to input,")
+print("and I will use those words to create a story.")
+proper_noun1=input("Enter a place. ")
+proper_noun2=input("Enter the name of a person. ")
+noun1=input("Enter a music genre. ")
+adverb1=input("Enter an adverb. ")
+adjective1=input("Enter an adjective. ")
+
+
+
+
+
+print(f"A day was passing in {proper_noun1}, and {proper_noun2} wanted to form a band. \n"
+f"It was formed, with their main genre being {noun1}. \n"
+"Soon they would play their first gig. \n"
+f"It went as {adverb1} as you would expect. \n"
+f"After this {adjective1} concert, the band members practiced more.")
