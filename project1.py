49f898107070bb3d6a7ae619fadde3b72c47230a
Martin <mwgoff24@students.bmchs.org>
2022-01-24 09:15:35 -0600

    finished project 1

--- project1.py
+++ project1.py
@@ -1,19 +1,23 @@
 print("A Mad Libbed Concert: The Maybe Funny Story")
 print("Welcome to this mad lib game!")
-print("I will ask for some words for you to input,")
-print("and I will use those words to create a story.")
+print("I will ask for some words for you to input, \n"
+"and I will create a story for you.")
 proper_noun1=input("Enter a place. ")
 proper_noun2=input("Enter the name of a person. ")
 noun1=input("Enter a music genre. ")
 adverb1=input("Enter an adverb. ")
 adjective1=input("Enter an adjective. ")
-
-
-
-
-
+verb1=input("Enter a present tense verb. ")
+adjective2=input("Enter a number. ")
+adjective3=input("Enter another number. ")
+adverb2=input("Enter another adverb. ")
+adjective4=input("Enter an adjective. ")
 print(f"A day was passing in {proper_noun1}, and {proper_noun2} wanted to form a band. \n"
 f"It was formed, with their main genre being {noun1}. \n"
 "Soon they would play their first gig. \n"
 f"It went as {adverb1} as you would expect. \n"
-f"After this {adjective1} concert, the band members practiced more.")
+f"After this {adjective1} concert, the band members practiced more. \n"
+f"Each member would {verb1} for four hours each day trying to get better. \n"
+f"Soon gig number {adjective2} would happen. \n"
+f"Each of the {adjective3} members had improved {adverb2}. \n"
+f"The audience members clapped, and the band knew how {adjective4} they had performed.")
